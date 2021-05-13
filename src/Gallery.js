c3669416fc01d4ec06a4af8d6be22ce32822bf5f
DC Lechok <dclechok@gmail.com>
2021-05-12 22:31:03 -0400

    importing images

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,14 +1,28 @@
 import React from 'react';
-
 import './Gallery.css';
+
+import imageOne from './images/img_1.jpg';
+// import imageOne from './images/img_1.jpg';
+// import imageOne from './images/img_1.jpg';
+// import imageOne from './images/img_1.jpg';
+// import imageOne from './images/img_1.jpg';
+// import imageOne from './images/img_1.jpg';
+// import imageOne from './images/img_1.jpg';
+// import imageOne from './images/img_1.jpg';
+// import imageOne from './images/img_1.jpg';
+// import imageOne from './images/img_1.jpg';
+// import imageOne from './images/img_1.jpg';
+// import imageOne from './images/img_1.jpg';
+
+
 
 function Gallery() {
     return (
         <main>
-            <div className="d-xl-flex justify-content-around">
+            <div className="d-flex justify-content-around">
                 <div className="row">
                     <div className="col-4 border">
-                        <img src="../images/img_1.jpg" alt="beach" />
+                        <img src={imageOne} alt="beach" />
                     </div>
                     <div class="col-4 border">
                         <img src="../images/img_2.jpg" alt="beach" />    
