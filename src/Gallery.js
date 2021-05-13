7c592a65d8ad1706080a04ba59adaba445c0a3b8
DC Lechok <dclechok@gmail.com>
2021-05-12 22:14:23 -0400

    added default images for gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,17 +1,20 @@
 import React from 'react';
+
 import './Gallery.css';
 
 function Gallery() {
     return (
         <main>
-            <p>Gallery coming soon!</p>
-            <div className="container">
+            <div className="d-xl-flex justify-content-around">
                 <div className="row">
-                    <div className="col-1 border">
-
-                    </div>
                     <div className="col-4 border">
-                        
+                        <img src="../images/img_1.jpg" alt="beach" />
+                    </div>
+                    <div class="col-4 border">
+                        <img src="../images/img_2.jpg" alt="beach" />    
+                    </div>
+                    <div class="col-4 border">
+                        <img src="../images/img_3.jpg" alt="beach" />    
                     </div>
                 </div>
             </div>
