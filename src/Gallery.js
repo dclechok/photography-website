fb8cb758eb35a9c6811a9b7fd066d12d58af8405
DC Lechok <dclechok@gmail.com>
2021-05-14 19:24:23 -0400

    adjusting flex box grid for main content

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Gallery.css';
 
 import imageOne from './images/img_1.jpg';
-// import imageOne from './images/img_1.jpg';
-// import imageOne from './images/img_1.jpg';
-// import imageOne from './images/img_1.jpg';
+import imageTwo from './images/img_2.jpg';
+import imageThree from './images/img_3.jpg';
+import imageFour from './images/img_4.jpg';
 // import imageOne from './images/img_1.jpg';
 // import imageOne from './images/img_1.jpg';
 // import imageOne from './images/img_1.jpg';
@@ -21,17 +21,21 @@ function Gallery() {
         <main>
             <div className="d-flex justify-content-around">
                 <div className="row">
-                    <div className="col-4 border">
+                    <div className="col-4 rmvPad">
                         <img src={imageOne} alt="beach" />
                     </div>
-                    <div class="col-4 border">
-                        <img src="../images/img_2.jpg" alt="beach" />    
+                    <div class="col-4 rmvPad">
+                        <img src={imageTwo} alt="beach" />    
                     </div>
-                    <div class="col-4 border">
-                        <img src="../images/img_3.jpg" alt="beach" />    
+                    <div class="col-4 rmvPad">
+                        <img src={imageThree} alt="beach" />    
+                    </div>
+                    <div className="col-4 rmvPad">
+                        <img src={imageFour} alt="beach" />
                     </div>
                 </div>
             </div>
+
         </main>
     );
 }
